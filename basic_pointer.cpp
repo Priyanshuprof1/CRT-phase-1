c1f73b64dff7892ae0a22f969eaee21e5d7188a1
Priyanshu Singh <priyanshu.prof1@gmail.com>
2026-05-05 22:46:47 +0530

    add my code

--- basic_pointer.cpp
+++ basic_pointer.cpp
@@ -18,14 +18,39 @@ return 0;
 
 #include<iostream>
 using namespace std;
-int billAmount(int &price){
-    int upd = price - (price*10/100);
+int billAmount(int *price){
+    int upd = *price - (*price*10/100);
     return upd;
 }
 int main(){
     int price=1000;
     cout<<price<<endl;
-    cout<<"updated price"<<billAmount(price)<<endl;
+    cout<<"updated price"<<billAmount(&price)<<endl;
     return 0;
 
 }
+
+// wap cpp program to swap two numbers using pointers
+
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+    cout<<"after swapping"<<endl;
+    cout<<"a: "<<*a<<endl;
+    cout<<"b: "<<*b<<endl;
+}
+int main(){
+    int x = 10;
+    int y = 20;
+    int *p = &x;
+    int *q = &y;
+    cout<<"before swapping"<<endl;
+    cout<<"a: "<<*p<<endl;
+    cout<<"b: "<<*q<<endl;
+    swap(&x,&y);
+    return 0;
+}
+
