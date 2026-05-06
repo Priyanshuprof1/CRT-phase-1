30bbb564ebcf1d4ca66863a8a6c902ff16f8e5b8
Priyanshu Singh <priyanshu.prof1@gmail.com>
2026-05-06 13:54:37 +0530

    add my code

---
+++ class.cpp
@@ -0,0 +1,19 @@
+// create a class named student
+#include <iostream>
+using namespace std;
+class Student {
+    public:
+    string name;
+    int age;
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+};
+int main(){
+    Student s1;
+    s1.name = "priyanshu";
+    s1.age = 20;
+    s1.display();
+    return 0;
+}
